b64ed07d2c141005a0bd10551144412a7e06d35f
Aryan khera <105739020+AryAnKhera23@users.noreply.github.com>
2024-03-08 02:31:52 +0530

    exit button

--- Assets/Scripts/LobbyController.cs
+++ Assets/Scripts/LobbyController.cs
@@ -8,14 +8,21 @@ using UnityEngine.UI;
 public class LobbyController : MonoBehaviour
 {
     [SerializeField] Button playButton;
+    [SerializeField] Button exitButton;
 
     private void Awake()
     {
         playButton.onClick.AddListener(Play);
+        exitButton.onClick.AddListener(QuitApplication);
     }
 
     private void Play()
     {
         SceneManager.LoadScene(1);
+    }
+
+    private void QuitApplication()
+    {
+        Application.Quit();
     }
 }
